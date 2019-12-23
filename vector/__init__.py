03e5a827c3f8f505560a40c6ab93d9b2a5f21757
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-12-23 15:14:36 +0100

    Add vector/__init__.py and vector/_version.py

---
+++ vector/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/vector for details.
+
+# Convenient access to the version number
+from ._version import __version__
---
+++ vector/_version.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2019, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/vector for details.
+
+__version__ = "0.0.0"
+
+version = __version__
+version_info = __version__.split(".")
