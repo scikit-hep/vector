67a45eed59317698ba83883a1042cce0f96749ca
Henry Schreiner <henryschreineriii@gmail.com>
2020-03-06 15:05:56 -0500

    Adding standard header

--- src/vector/__init__.py
+++ src/vector/__init__.py
@@ -1,7 +1,9 @@
-# Copyright (c) 2019-2020, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
+# Copyright (c) 2019-2020, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/vector for details.
+
+from __future__ import division, absolute_import, print_function
 
 # Convenient access to the version number
 from .version import version as __version__
--- src/vector/numba/awkward.py
+++ src/vector/numba/awkward.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2019-2020, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/vector for details.
+
+from __future__ import division, absolute_import, print_function
+
 import awkward1 as ak
 from .lorentz import LorentzXYZType
 from ..awkward import lorentzbehavior
