08a49c51cf4e2b8c3e63ee99c7a8c48c91e02892
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2020-03-06 13:56:26 -0500

    Adding numba backend
    Co-authored-by: Jim Pivarski <jpivarski@gmail.com>

---
+++ tests/numba_tests/test_basic.py
@@ -0,0 +1,45 @@
+import pytest
+
+import numba
+from vector.numba.lorentz import LorentzXYZFree
+
+
+@numba.njit
+def pass_through(obj):
+    return obj
+
+
+def test_simple_check():
+    testit = LorentzXYZFree(1, 2, 3, 4)
+    assert repr(testit) == "Lxyz(1 2 3 4)"
+
+
+def test_passthrough():
+    assert repr(pass_through(LorentzXYZFree(1, 2, 3, 4))) == repr(
+        LorentzXYZFree(1.0, 2.0, 3.0, 4.0)
+    )
+
+
+def test_constructor():
+    @numba.njit
+    def test_constructor():
+        return LorentzXYZFree(1.1, 2.2, 3.3, 4.4)
+
+    assert repr(test_constructor()) == repr(LorentzXYZFree(1.1, 2.2, 3.3, 4.4))
+
+
+@numba.njit
+def try_it_out(testit):
+    return testit.x, testit["x"], testit.pt, testit.eta, testit.phi, testit.mass
+
+
+def test_free_elements():
+    testit = LorentzXYZFree(1, 2, 3, 4)
+    x1, x2, pt, eta, phi, mass = try_it_out(testit)
+
+    assert testit.x == x1
+    assert testit["x"] == x2
+    assert testit.pt == pt
+    assert testit.eta == eta
+    assert testit.phi == phi
+    assert testit.mass == mass
