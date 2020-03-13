eba47c5b7bce0b33c0e733c9d4fc56719fd25b6c
Henry Schreiner <henryschreineriii@gmail.com>
2020-03-13 16:55:26 -0400

    Update ROOT test

--- tests/root_tests/test_root_basic.py
+++ tests/root_tests/test_root_basic.py
@@ -1,4 +1,19 @@
-def test_import_root():
-    import ROOT
+from pytest import approx
 
-    assert ROOT.__file__ is not None
+import ROOT
+
+from vector.single.lorentz import LorentzXYZFree
+
+
+def test_simple():
+    v1 = LorentzXYZFree(1, 2, 3, 4)
+    v2 = ROOT.TLorentzVector(1, 2, 3, 4)
+
+    assert v1.x == approx(v2.X())
+    assert v1.y == approx(v2.Y())
+    assert v1.z == approx(v2.Z())
+    assert v1.t == approx(v2.T())
+    assert v1.pt == approx(v2.Pt())
+    assert v1.eta == approx(v2.Eta())
+    assert v1.phi == approx(v2.Phi())
+    assert v1.mass == approx(v2.M())
