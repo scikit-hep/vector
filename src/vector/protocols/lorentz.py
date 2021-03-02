831c36aa957a0650207a748b372ef98e59a1929e
Henry Schreiner <henryschreineriii@gmail.com>
2021-03-02 16:59:02 -0500

    fix: support 3.6 again

--- src/vector/protocols/lorentz.py
+++ src/vector/protocols/lorentz.py
@@ -1,4 +1,10 @@
-from typing import Protocol, Tuple, TypeVar, overload
+import sys
+from typing import Tuple, TypeVar, overload
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
 
 Self = TypeVar("Self", bound="Scalar")
 
