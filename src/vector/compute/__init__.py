f09045ebccbf75f1f0ae3e7c291f7be1e38c6602
Jim Pivarski <jpivarski@gmail.com>
2021-03-03 18:36:47 -0600

    Only planar computes exist at this point.

--- src/vector/compute/__init__.py
+++ src/vector/compute/__init__.py
@@ -3,6 +3,6 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/vector for details.
 
-import vector.compute.lorentz  # noqa: F401
+# import vector.compute.lorentz  # noqa: F401
 import vector.compute.planar  # noqa: F401
-import vector.compute.spatial  # noqa: F401
+# import vector.compute.spatial  # noqa: F401
