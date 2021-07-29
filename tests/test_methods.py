3b234e6f102874e91c8401796d064e82d37729ac
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-29 13:55:57 +0200

    Add _handler_of func. test

---
+++ tests/test_methods.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2019-2021, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/vector for details.
+
+import vector
+
+
+class CustomVector(vector.VectorObject4D):
+    pass
+
+
+def test_handler_of():
+    object_a = CustomVector.from_xyzt(0.0, 0.0, 0.0, 0.0)
+    object_b = CustomVector.from_xyzt(1.0, 1.0, 1.0, 1.0)
+    protocol = vector._methods._handler_of(object_a, object_b)
+    assert protocol == object_a
