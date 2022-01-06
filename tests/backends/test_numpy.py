c207505cbdc17b2c344bcf72891c8efbaee472b5
Florian Bruggisser <florian@bildspur.ch>
2022-01-06 19:28:45 +0100

    VectorNumpy pickle suppport to enable multiprocessing (#163)
    * added VectorNumpy pickle tests
    
    * added MomentumNumpy pickle tests
    
    * implemented reduce method to preserve custom attributes when pickling VectorNumpy object
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added return types and fixed tuple concatenation
    
    * added stronger type annotations
    
    * fixed annotations to match super class
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/backends/test_numpy.py
+++ tests/backends/test_numpy.py
@@ -4,6 +4,7 @@
 # or https://github.com/scikit-hep/vector for details.
 
 import math
+import pickle
 
 import numpy
 
@@ -29,3 +30,99 @@ def test_rhophi():
     assert numpy.allclose(array.y, [0, 1, 4])
     assert numpy.allclose(array.rho, [0, 1, 5])
     assert numpy.allclose(array.phi, [10, math.atan2(1, 0), math.atan2(4, 3)])
+
+
+def test_pickle_vector_numpy_2d():
+    array = vector._backends.numpy_.VectorNumpy2D(
+        [(0, 0), (0, 1), (3, 4)], dtype=[("x", numpy.float64), ("y", numpy.float64)]
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.x, array.x)
+    assert numpy.allclose(array_new.y, array.y)
+
+
+def test_pickle_momentum_numpy_2d():
+    array = vector._backends.numpy_.MomentumNumpy2D(
+        [(0, 0), (0, 1), (3, 4)], dtype=[("rho", numpy.float64), ("phi", numpy.float64)]
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.rho, array.rho)
+    assert numpy.allclose(array_new.phi, array.phi)
+
+
+def test_pickle_vector_numpy_3d():
+    array = vector._backends.numpy_.VectorNumpy3D(
+        [(0, 0, 0), (0, 1, 1), (3, 4, 5)],
+        dtype=[("x", numpy.float64), ("y", numpy.float64), ("z", numpy.float64)],
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.x, array.x)
+    assert numpy.allclose(array_new.y, array.y)
+    assert numpy.allclose(array_new.z, array.z)
+
+
+def test_pickle_momentum_numpy_3d():
+    array = vector._backends.numpy_.MomentumNumpy3D(
+        [(0, 0, 0), (0, 1, 1), (3, 4, 5)],
+        dtype=[
+            ("rho", numpy.float64),
+            ("phi", numpy.float64),
+            ("theta", numpy.float64),
+        ],
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.rho, array.rho)
+    assert numpy.allclose(array_new.phi, array.phi)
+    assert numpy.allclose(array_new.theta, array.theta)
+
+
+def test_pickle_vector_numpy_4d():
+    array = vector._backends.numpy_.VectorNumpy4D(
+        [(0, 0, 0, 0), (0, 1, 1, 1), (3, 4, 5, 6)],
+        dtype=[
+            ("x", numpy.float64),
+            ("y", numpy.float64),
+            ("z", numpy.float64),
+            ("t", numpy.float64),
+        ],
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.x, array.x)
+    assert numpy.allclose(array_new.y, array.y)
+    assert numpy.allclose(array_new.z, array.z)
+    assert numpy.allclose(array_new.t, array.t)
+
+
+def test_pickle_momentum_numpy_4d():
+    array = vector._backends.numpy_.MomentumNumpy4D(
+        [(0, 0, 0, 0), (0, 1, 1, 1), (3, 4, 5, 6)],
+        dtype=[
+            ("rho", numpy.float64),
+            ("phi", numpy.float64),
+            ("theta", numpy.float64),
+            ("tau", numpy.float64),
+        ],
+    )
+
+    array_pickled = pickle.dumps(array)
+    array_new = pickle.loads(array_pickled)
+
+    assert numpy.allclose(array_new.rho, array.rho)
+    assert numpy.allclose(array_new.phi, array.phi)
+    assert numpy.allclose(array_new.theta, array.theta)
+    assert numpy.allclose(array_new.tau, array.tau)
