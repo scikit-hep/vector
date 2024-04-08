55815a9b15b3f6da1c608338551e49b7e7ec6d6d
Saransh Chopra <saransh0701@gmail.com>
2024-04-08 16:58:37 +0200

    fix deltaphi tests

--- tests/compute/sympy/planar/test_deltaphi.py
+++ tests/compute/sympy/planar/test_deltaphi.py
@@ -1,52 +1,56 @@
-# # Copyright (c) 2019-2024, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
-# #
-# # Distributed under the 3-clause BSD license, see accompanying file LICENSE
-# # or https://github.com/scikit-hep/vector for details.
+# Copyright (c) 2019-2024, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/vector for details.
 
-# from __future__ import annotations
+from __future__ import annotations
 
-# import math
+import pytest
 
-# import pytest
+import vector
 
-# import vector.backends.object
+sympy = pytest.importorskip("sympy")
 
-
-# def test_xy_xy():
-#     v1 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectXY(1, 2)
-#     )
-#     v2 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectXY(3, 4)
-#     )
-#     assert v1.deltaphi(v2) == pytest.approx(math.atan2(2, 1) - math.atan2(4, 3))
+x, y, nx, ny = sympy.symbols("x y nx ny")
 
 
-# def test_xy_rhophi():
-#     v1 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectXY(1, 2)
-#     )
-#     v2 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectRhoPhi(3, 4)
-#     )
-#     assert v1.deltaphi(v2) == pytest.approx(math.atan2(2, 1) - 4)
+def test_xy_xy():
+    v1 = vector.VectorSympy2D(azimuthal=vector.backends.sympy.AzimuthalSympyXY(x, y))
+    v2 = vector.VectorSympy2D(azimuthal=vector.backends.sympy.AzimuthalSympyXY(nx, ny))
+    assert (
+        v1.deltaphi(v2)
+        == sympy.Mod(-sympy.atan2(ny, nx) + sympy.atan2(y, x) + sympy.pi, 2 * sympy.pi)
+        - sympy.pi
+    )
 
 
-# def test_rhophi_xy():
-#     v1 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectRhoPhi(1, 2)
-#     )
-#     v2 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectXY(3, 4)
-#     )
-#     assert v1.deltaphi(v2) == pytest.approx(2 - math.atan2(4, 3))
+def test_xy_rhophi():
+    v1 = vector.VectorSympy2D(azimuthal=vector.backends.sympy.AzimuthalSympyXY(x, y))
+    v2 = vector.VectorSympy2D(
+        azimuthal=vector.backends.sympy.AzimuthalSympyRhoPhi(nx, ny)
+    )
+    assert (
+        v1.deltaphi(v2)
+        == sympy.Mod(-ny + sympy.atan2(y, x) + sympy.pi, 2 * sympy.pi) - sympy.pi
+    )
 
 
-# def test_rhophi_rhophi():
-#     v1 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectRhoPhi(1, 2)
-#     )
-#     v2 = vector.backends.object.VectorObject2D(
-#         azimuthal=vector.backends.object.AzimuthalObjectRhoPhi(3, 4)
-#     )
-#     assert v1.deltaphi(v2) == pytest.approx(2 - 4)
+def test_rhophi_xy():
+    v1 = vector.VectorSympy2D(
+        azimuthal=vector.backends.sympy.AzimuthalSympyRhoPhi(x, y)
+    )
+    v2 = vector.VectorSympy2D(azimuthal=vector.backends.sympy.AzimuthalSympyXY(nx, ny))
+    assert (
+        v1.deltaphi(v2)
+        == sympy.Mod(y - sympy.atan2(ny, nx) + sympy.pi, 2 * sympy.pi) - sympy.pi
+    )
+
+
+def test_rhophi_rhophi():
+    v1 = vector.VectorSympy2D(
+        azimuthal=vector.backends.sympy.AzimuthalSympyRhoPhi(x, y)
+    )
+    v2 = vector.VectorSympy2D(
+        azimuthal=vector.backends.sympy.AzimuthalSympyRhoPhi(nx, ny)
+    )
+    assert v1.deltaphi(v2) == sympy.Mod(-ny + y + sympy.pi, 2 * sympy.pi) - sympy.pi
