553564a517a519f983385cadd2c1c2a363a86e2d
Saransh Chopra <saransh0701@gmail.com>
2024-04-17 15:35:19 +0200

    Simpler values

--- tests/compute/lorentz/test_is_lightlike.py
+++ tests/compute/lorentz/test_is_lightlike.py
@@ -5,198 +5,196 @@
 
 from __future__ import annotations
 
-import math
-
 import vector.backends.object
 
 
 def test_xy_z_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_xy_z_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectTau(1.7320508075688772),
     )
     assert not vec.is_lightlike()
 
 
 def test_xy_theta_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_xy_theta_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectTau(1.7320508075688772),
     )
     assert not vec.is_lightlike()
 
 
 def test_xy_eta_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_xy_eta_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectXY(3, 4),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectXY(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectTau(1.7320508075688772),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_z_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_z_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectZ(20),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectZ(0),
+        vector.backends.object.TemporalObjectTau(-4.58257569495584),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_theta_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_theta_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectTheta(0.24497866312686423),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectTheta(1.5707963267948966),
+        vector.backends.object.TemporalObjectTau(-4.58257569495584),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_eta_t():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectT(math.sqrt(425)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectT(1),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectT(math.sqrt(400)),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectT(2),
     )
     assert not vec.is_lightlike()
 
 
 def test_rhophi_eta_tau():
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectTau(2.384185791015625e-07),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectTau(0),
     )
     assert vec.is_lightlike()
 
     vec = vector.backends.object.VectorObject4D(
-        vector.backends.object.AzimuthalObjectRhoPhi(5, 0),
-        vector.backends.object.LongitudinalObjectEta(2.0947125472611012),
-        vector.backends.object.TemporalObjectTau(-5),
+        vector.backends.object.AzimuthalObjectRhoPhi(1, 0),
+        vector.backends.object.LongitudinalObjectEta(0),
+        vector.backends.object.TemporalObjectTau(-4.58257569495584),
     )
     assert not vec.is_lightlike()
