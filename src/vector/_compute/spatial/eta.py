e161dbfb641a1ee6c094ba74f286c839086fa311
Saransh Chopra <saransh0701@gmail.com>
2025-06-10 22:53:25 +0200

    chore: update copyright year and text (match with JOSS) (#606)

--- src/vector/_compute/spatial/eta.py
+++ src/vector/_compute/spatial/eta.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2024, Jonas Eschle, Jim Pivarski, Eduardo Rodrigues, and Henry Schreiner.
+# Copyright (c) 2019-2025, Saransh Chopra, Henry Schreiner, Eduardo Rodrigues, Jonas Eschle, and Jim Pivarski.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/vector for details.
