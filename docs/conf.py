5817e42116fc918de7ab34ca4c4e7f3a3e5190f6
Saransh Chopra <saransh0701@gmail.com>
2025-08-31 23:32:19 +0100

    docs: fix edit button link

--- docs/conf.py
+++ docs/conf.py
@@ -85,6 +85,7 @@ html_theme_options = {
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
+    "path_to_docs": "docs/",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
