c535be122814639e125bd42ffd723b5f4c4a85d8
Saransh Chopra <saransh0701@gmail.com>
2026-01-01 23:51:52 +0530

    docs: copyright should only include the year of creation (#674)

--- tests/compute/sympy/spatial/test_cottheta.py
+++ tests/compute/sympy/spatial/test_cottheta.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2025, Saransh Chopra, Henry Schreiner, Eduardo Rodrigues, Jonas Eschle, and Jim Pivarski.
+# Copyright (c) 2019, Saransh Chopra, Henry Schreiner, Eduardo Rodrigues, Jonas Eschle, and Jim Pivarski.
 #
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/vector for details.
